77388a7830ea767aa2bbef87cfcd22a4935f081f
krshxcx <krshxcx@gmail.com>
2026-03-25 12:03:32 +0530

    area and perimeter of rectangle

---
+++ AreaRect.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class AreaRect {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter length of the rectangle: ");
+        int length = sc.nextInt(); // Read length from user
+        System.out.print("Enter breadth of the rectangle: ");   
+        int breadth = sc.nextInt(); // Read breadth from user
+        
+        int area = length * breadth; // Calculate area of rectangle
+        System.out.println("Area of the rectangle: " + area);
+
+        int perimeter = 2 * (length + breadth); // Calculate perimeter of rectangle
+        System.out.println("Perimeter of the rectangle: " + perimeter);
+            
+        }
+    }
+}
