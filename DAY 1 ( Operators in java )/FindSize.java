db0434472a0ffb908f5d46b0c9046b74c27b477b
krshxcx <krshxcx@gmail.com>
2026-03-25 11:33:16 +0530

    finding the size of int, float, double, long using Integer.BYTES, Float.BYTES etc. in java

---
+++ DAY 1 ( Operators in java )/FindSize.java
@@ -0,0 +1,10 @@
+public class FindSize {
+    public static void main(String[] args){
+        System.out.println("Size of byte: " + Byte.BYTES + " bits");
+        System.out.println("Size of short: " + Short.BYTES + " bits");
+        System.out.println("Size of int: " + Integer.BYTES + " bits");
+        System.out.println("Size of long: " + Long.BYTES + " bits");
+        System.out.println("Size of float: " + Float.BYTES + " bits");
+        System.out.println("Size of double: " + Double.BYTES + " bits");
+    }
+}
