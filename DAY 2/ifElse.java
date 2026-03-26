3c6746aac3bf0b1df74484a5f657ab3e362c0d73
krshxcx <krshxcx@gmail.com>
2026-03-26 10:25:24 +0530

    conditional statements

---
+++ DAY 2/ifElse.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ifElse{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            System.out.println("Enter your age:");
+            int age = sc.nextInt();
+
+            if (age < 18){
+                System.out.println("You are a minor.");
+            } else if (age >= 18 && age < 65) {
+                System.out.println("You are an adult.");
+            } else {
+                System.out.println("You are a senior citizen.");
+            }
+        }
+    } 
+}
